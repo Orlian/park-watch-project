df063a8efd5c3725b85f413eec447353a933b392
Joonas <joonas.lehtoranta@live.fi>
2021-10-29 11:35:09 +0300

    Update Navbar.js

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {AppBar, Toolbar} from '@mui/material';
+import {AppBar, Container, Toolbar} from '@mui/material';
 import {Link} from 'react-router-dom';
 import SettingsIcon from '@mui/icons-material/Settings';
 
@@ -7,8 +7,10 @@ const Navbar = () => {
   return (
       <AppBar color='primary' sx={{position: 'sticky'}}>
         <Toolbar>
+          <Container disableGutters={true} sx={{display: 'flex', justifyContent: 'space-between'}}>
           <Link to={'/'}>Home</Link>
           <Link to={'/preferences'}><SettingsIcon /></Link>
+          </Container>
         </Toolbar>
       </AppBar>
   )
